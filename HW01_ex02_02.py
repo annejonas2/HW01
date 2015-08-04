a0086a11733ce01ed3175d3d2a1f457f7c48e706
annejonas <ajonas@ischool.berkeley.edu>
2015-08-04 16:52:15 -0700

    ch2ex2 assignment

--- HW01_ex02_02.py
+++ HW01_ex02_02.py
@@ -9,25 +9,25 @@
 # and the type (of the value of the expression).
 
 # 1. width/2
-# value:
-# type:
+# value: 8	
+# type: integer
 
 # 2. width/2.0
-# value:
-# type:
+# value: 8.5
+# type: floating-point
 
 # 3. height/3
-# value:
-# type:
+# value: 4.0
+# type: floating point
 
 # 4. 1 + 2 * 5
-# value:
-# type:
+# value: 11
+# type: integer
 
 # 5. delimiter * 5
-# value:
-# type:
+# value:.....
+# type: string
 # 
 # Use the Python interpreter to check your answers.
 # [comment on any surprises here]
-# 
+# I was surprised that height/3 was 4.0 rather than 4 - even though the height was a floating point I expected that in the result it would give an integer.
